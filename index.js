e7b2fbd7f20ccf2c0498c8fe01b5aa9862809d84
Ubuntu <ubuntu@ip-172-31-46-192.us-west-2.compute.internal>
2013-11-18 21:58:31 +0000

    added 1st version of index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+var port = 8080;
+
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+
+
+
+app.get("/", function(req, res){
+    res.render("page");
+});
+
+app.listen(port);
+console.log("Listening on port " + port);
